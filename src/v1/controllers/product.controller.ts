bf22f4c6e16caa968321b48e0b9cdc04087520aa
Ndanyuzwe Duncan <semunda9@gmail.com>
2024-10-24 23:58:48 +0200

    create is working

--- src/v1/controllers/product.controller.ts
+++ src/v1/controllers/product.controller.ts
@@ -17,7 +17,7 @@ export default class ProductController {
     const productExists = await prisma.products.findUnique({
       where: { Name: Name },
     });
-    if (productExists) return res.status(200).json("It exists");
+    if (productExists) return res.status(200).json("The name must be unique");
     console.log(productExists);
 
     const product = await prisma.products.create({
@@ -25,5 +25,22 @@ export default class ProductController {
     });
     console.log(product);
     return res.status(201).json(product);
+  });
+
+  static updateOneProduct = catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const { ...data } = req.body;
+    const productExists = await prisma.products.findFirst({
+      where: { ID: id },
+    });
+    if (!productExists)
+      return res.status(404).json("The product to be updated is not present");
+
+    const updateProduct = await prisma.products.update({
+      where: { ID: id },
+      data: { ...data },
+    });
+
+    return res.status(200).json({ message: "Updated", data: updateProduct });
   });
 }
--- src/v1/routes/product.route.ts
+++ src/v1/routes/product.route.ts
@@ -2,15 +2,19 @@ import { Router } from "express";
 import ProductController from "../controllers/product.controller";
 import validate from "../../middlewares/validate";
 import { createProduct } from "../validations/product.validation";
+import route from ".";
 
 const router: Router = Router();
 
 // Use POST for creating a new product
+router.get("/:id", ProductController.updateOneProduct);
+
 router.post(
   "/",
   validate(createProduct.body),
   ProductController.createOneProduct
 );
+
 router.get("/", (req, res) => {
   res.json("Hi");
 });
